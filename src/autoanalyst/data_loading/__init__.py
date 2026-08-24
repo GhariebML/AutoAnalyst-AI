4389322733c099a8efadcbb744d323e426895821
Aya Mostafa <aya091760@gmail.com>
2026-08-24 14:28:24 +0300

    Add files via upload

--- src/autoanalyst/data_loading/__init__.py
+++ src/autoanalyst/data_loading/__init__.py
@@ -1,1 +1,15 @@
-"""Data Loading module for AutoAnalyst AI."""
+"""Data profiling initialization."""
+
+from autoanalyst.data_profiling.profiler import (
+    generate_basic_profile,
+    get_duplicate_count,
+    get_missing_values_report,
+    DataProfilingAgent,
+)
+
+__all__ = [
+    "generate_basic_profile",
+    "get_duplicate_count",
+    "get_missing_values_report",
+    "DataProfilingAgent",
+]
